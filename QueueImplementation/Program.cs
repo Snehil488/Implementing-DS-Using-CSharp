b4275c9b1fee86626b3622a1c8d22df3da8b0d3f
Snehil <snehilverma488@gmail.com>
2020-10-21 12:11:52 +0530

    [Snehil] Add : Queue Implementation Using array

---
+++ QueueImplementation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QueueImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.enQueue(1);
+            queue.enQueue(2);
+            queue.enQueue(3);
+            queue.enQueue(4);
+            queue.DisplayQueue();
+            queue.deQueue();
+            Console.WriteLine("\nAfter one deQueue Operation");
+            queue.DisplayQueue();
+        }
+    }
+}
---
+++ QueueImplementation/Queue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueImplementation
+{
+    class Queue
+    {
+        int[] queue = new int[5];
+        int front = -1;
+        int rear = -1;
+        int size = 0;
+
+        public void enQueue(int data)
+        {
+            if(rear == -1)
+            {
+                front = 0;
+                rear = 0;
+                queue[rear] = data;
+                rear++;
+                size++;
+            }
+            else
+            {
+                queue[rear] = data;
+                rear++;
+                size++;
+            }
+        }
+
+        public int deQueue()
+        {
+            int value = 0;
+            if(front == -1)
+            {
+                Console.WriteLine("Empty Queue");
+            }
+            else
+            {
+                value = queue[front];
+                front++;
+                size--;
+            }
+            return value;
+        }
+
+        public void DisplayQueue()
+        {
+            Console.Write("Queue : ");
+            for(int i=0; i < size; i++)
+            {
+                Console.Write(queue[i]+" ");
+            }
+        }
+    }
+}

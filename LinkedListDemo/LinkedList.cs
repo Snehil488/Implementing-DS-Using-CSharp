c73fcd351aea3de10a1b72aef9a84b0a4128ce1a
Snehil <snehilverma488@gmail.com>
2020-10-21 12:10:07 +0530

    [Snehil] Add : LinkedList Implementation

---
+++ LinkedListDemo/LinkedList.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListDemo
+{
+    class LinkedList
+    {
+        Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node n = head;
+                while(n.next != null)
+                {
+                    n = n.next;
+                }
+                n.next = node;
+            }
+            Console.WriteLine(node.data + " Inserted into LinkedList");
+        }
+
+        public void Display()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+            }
+            else
+            {
+                Node n = head;
+                while (n.next != null)
+                {
+                    Console.Write(n.data + " ");
+                    n = n.next;
+                }
+                Console.Write(n.data + "\n");
+            }
+        }
+
+        //starting position is 1
+        public void InsertAtParticularPosition(int position, int data)
+        {
+            Node node = new Node(data);
+            if (position < 1)
+            {
+                Console.WriteLine("Invalid Index");
+            }
+            else if(position == 1)
+            {
+                node.next = head;
+                head = node;
+            }
+            else
+            {
+                //for inserting at position 2 previous node will be head otherwise it is position-2
+                Node n = head, prev = head, after;
+                for(int pos = 1; pos < position; pos++)
+                {
+                    n = n.next;
+                    if (pos == position - 2)
+                    {
+                        prev = n;
+                    }   
+                }
+                after = n;
+                prev.next = node;
+                node.next = after;
+            }
+        }
+
+        public Node RemoveFirstNode()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("Empty LinkedList");
+                return null;
+            }
+            else
+            {
+                Node n = head;
+                head = head.next;
+                return n;
+            }
+        }
+        
+        public Node RemoveLastNode()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Empty LinkedList");
+                return null;
+            }
+            else
+            {
+                Node n = head;
+                while(n.next.next != null)
+                {
+                    n = n.next;
+                }
+                Node removedNode = n.next;
+                n.next = null;
+                return removedNode;
+            }
+        }
+
+        public Node Search(int data)
+        {
+            Node n = head;
+            if(head == null)
+            {
+                Console.WriteLine("LinkedList Empty.");
+                return null;
+            }
+            if(head.data == data)
+            {
+                return head;
+            }
+            else
+            {
+                while(n.next != null)
+                {
+                    n = n.next;
+                    if(n.data == data)
+                    {
+                        return n;
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ LinkedListDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(10);
+            linkedList.Add(20);
+            linkedList.Add(30);
+            linkedList.Add(40);
+            linkedList.Add(60);
+            linkedList.Display();
+            Console.WriteLine("-------------");
+            //linkedList.InsertAtParticularPosition(5, 50);
+            //linkedList.Display();
+            //linkedList.RemoveFirstNode();
+            //linkedList.Display();
+            //linkedList.RemoveLastNode();
+            //linkedList.Display();
+            //Console.WriteLine(linkedList.Search(60).data);
+        }
+    }
+}

c67ce4532fcde8083bf37d167d05ae190b843897
Snehil <snehilverma488@gmail.com>
2020-10-21 12:10:57 +0530

    [Snehil] Add : Stack Implementation Using array

---
+++ StackImplementation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+            stack.push(4);
+            stack.DisplayStack();
+            stack.pop();
+            Console.WriteLine("After One pop operation");
+            stack.DisplayStack();
+        }
+    }
+}
---
+++ StackImplementation/Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackImplementation
+{
+    class Stack
+    {
+        int[] stack = new int[5];
+        int top = -1;
+
+        public void push(int data)
+        {
+            if(top == -1)
+            {
+                top = 0;
+                stack[top] = data;
+            }
+            else
+            {
+                top++;
+                stack[top] = data;
+            }
+        }
+
+        public int pop()
+        {
+            int value = 0;
+            if(top == -1)
+            {
+                Console.WriteLine("Stack Empty.");
+            }
+            else
+            {
+                value = stack[top];
+                stack[top] = 0;
+                top--;
+            }
+            return value;
+        }
+
+        public void DisplayStack()
+        {
+            for(int i =top; i>=0; i--)
+            {
+                Console.WriteLine(stack[i] + "\n" + "--");
+            }
+        }
+    }
+}

a6250307e2c1966da21d067c748db6f12b59b6ac
Snehil <snehilverma488@gmail.com>
2020-10-22 09:41:49 +0530

    [Snehil] Add : Stack Implementation Using Linked List

---
+++ StackImplementationUsingLinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StackUsingLinkedListLive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackUsingLinkedList Stack = new StackUsingLinkedList();
+            Stack.Push(70);
+            Stack.Push(30);
+            Stack.Push(56);
+            Stack.Display();
+            //linkedListStack.Peek();
+            //linkedListStack.Pop();
+            //Stack.IsEmpty();
+            //Stack.Display();
+
+        }
+    }
+}
